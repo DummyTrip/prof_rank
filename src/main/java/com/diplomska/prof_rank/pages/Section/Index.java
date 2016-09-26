06d283a76403309b28deb841097575db64455515
DummyTrip <mladoprle@yahoo.com>
2016-09-26 12:47:07 +0200

    Display rulebookSections in section/index.

--- src/main/java/com/diplomska/prof_rank/pages/Section/Index.java
+++ src/main/java/com/diplomska/prof_rank/pages/Section/Index.java
@@ -2,6 +2,7 @@ package com.diplomska.prof_rank.pages.Section;
 
 import com.diplomska.prof_rank.entities.RulebookSection;
 import com.diplomska.prof_rank.entities.Section;
+import com.diplomska.prof_rank.services.RulebookHibernate;
 import com.diplomska.prof_rank.services.SectionHibernate;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.beaneditor.BeanModel;
@@ -20,11 +21,20 @@ public class Index {
     @Inject
     private SectionHibernate sectionHibernate;
 
+    @Inject
+    private RulebookHibernate rulebookHibernate;
+
     @Property
     private RulebookSection rulebookSection;
 
     @Property
-    private BeanModel<RulebookSection> sectionBeanModel;
+    private Section section;
+
+    @Property
+    private BeanModel<RulebookSection> rulebookSectionBeanModel;
+
+    @Property
+    private BeanModel<Section> sectionBeanModel;
 
     @Inject
     private BeanModelSource beanModelSource;
@@ -35,21 +45,35 @@ public class Index {
     @Inject
     private PropertyConduitSource pcs;
 
+    public List<Section> getSections() {
+        return sectionHibernate.getAll();
+    }
+
     public List<RulebookSection> getRulebookSections() {
         return sectionHibernate.getAllRulebookSection();
     }
 
     void setupRender() {
-        sectionBeanModel = beanModelSource.createDisplayModel(RulebookSection.class, messages);
-//        sectionBeanModel.add("rulebookName", pcs.create(RulebookSection.class, "rulebook.name"));
-        sectionBeanModel.add("sectionName", pcs.create(RulebookSection.class, "section.name"));
-        sectionBeanModel.add("show", null);
+        sectionBeanModel = beanModelSource.createDisplayModel(Section.class, messages);
         sectionBeanModel.add("edit", null);
         sectionBeanModel.add("delete", null);
+
+        rulebookSectionBeanModel = beanModelSource.createDisplayModel(RulebookSection.class, messages);
+        rulebookSectionBeanModel.add("rulebookName", pcs.create(RulebookSection.class, "rulebook.name"));
+        rulebookSectionBeanModel.add("sectionName", pcs.create(RulebookSection.class, "section.name"));
+        rulebookSectionBeanModel.add("show", null);
+        rulebookSectionBeanModel.add("delete", null);
     }
 
     @CommitAfter
-    void onActionFromDelete(Long sectionId) {
+    void onActionFromDelete(Long id) {
+        RulebookSection entity = sectionHibernate.getRulebookSectionById(id);
+
+        rulebookHibernate.deleteSection(entity.getRulebook(), entity.getSection());
+    }
+
+    @CommitAfter
+    void onActionFromDeleteSection(Long sectionId) {
         Section entity = sectionHibernate.getById(sectionId);
         sectionHibernate.delete(entity);
     }
